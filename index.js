7a7a45f791932eb478fe57ce104725c4d73b3e72
dnmtr <34169713+danMateer@users.noreply.github.com>
2020-04-15 12:09:16 -0500

    Update-2

--- index.js
+++ index.js
@@ -28,6 +28,12 @@ export default class Home extends Component {
                 className="form-control"
                 ref={ref => (this._input = ref)}
               />
+              <input
+                type="text"
+                placeholder="Enter a GitLab user..."
+                className="form-control"
+                ref={ref => (this._input = ref)}
+              />
             </div>
           </div>
           <button type="submit" className="btn btn-primary">
